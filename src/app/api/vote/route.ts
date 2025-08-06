d7f32992379be503710e15dfde8b073e8a7796f7
ritanchmehta <ritanch0001@gmail.com>
2025-08-06 11:10:10 +0530

    corrected the vote api

--- src/app/api/vote/route.ts
+++ src/app/api/vote/route.ts
@@ -1,7 +1,6 @@
 import { answerCollection, db, questionCollection, voteCollection } from "@/models/name";
 import { databases, users } from "@/models/server/config";
 import { UserPrefs } from "@/store/Auth";
-import { unique } from "next/dist/build/utils";
 import { NextRequest, NextResponse } from "next/server";
 import { ID, Query } from "node-appwrite";
 
@@ -9,56 +8,77 @@ export async function POST(request: NextRequest){
     try {
         //grab the data
         const {votedById, voteStatus, type, typeId} = await request.json()
-        //list-document
-        const response = await databases.listDocuments(
+        //check existing vote
+        const existingVoteRes = await databases.listDocuments(
             db, voteCollection, [
                 Query.equal("type",type),
                 Query.equal("typeId",typeId),
                 Query.equal("votedById",votedById),
-                Query.equal("voteStatus",voteStatus),
             ]
-        )
-        console.log(response);
+        ) //Query.equal is ANDing all the results and giving the matching document
+        console.log(existingVoteRes);
+
+        const existingVote = existingVoteRes.documents[0];
+
+        // Fetch Author
+        const questionOrAnswer = await databases.getDocument(
+            db,
+            type === "question" ? questionCollection : answerCollection,
+            typeId
+        );
+
+        const authorId = questionOrAnswer.authorId;
+        const authorPrefs = await users.getPrefs<UserPrefs>(authorId);
+
+        let doc=null;
+        let voteAction = "Voted"
         
-        //vote exist
-        if(response.documents.length>0){
-            await databases.deleteDocument(db, voteCollection, response.documents[0].$id)
+        
+        if(existingVote){
+            // If same vote - unvote
+            if (existingVote.voteStatus === voteStatus){
+                await databases.deleteDocument(db, voteCollection, existingVote.$id);
 
-            // increase or decrease the reputation
-            const questionOrAnswer = await databases.getDocument(
-                db,
-                type === "question" ? questionCollection : answerCollection,
-                typeId
-            );
-
-            const authorPrefs = await users.getPrefs<UserPrefs>(questionOrAnswer.authorId)
-
-            await users.updatePrefs<UserPrefs>(questionOrAnswer.authorId, {
-                reputation: response.documents[0].voteStatus === "upvoted" ? Number(authorPrefs.reputation) -1 : Number(authorPrefs.reputation) + 1
-            })
-        }
-
-        //that means previous vote doesn't exist or vote status changes
-        if(response.documents[0]?.voteStatus !== voteStatus){
-            const doc = await databases.createDocument(db, voteCollection, ID.unique(), {
+            await users.updatePrefs<UserPrefs>(authorId, {
+                reputation: 
+                    existingVote.voteStatus === "upvoted" 
+                    ? Number(authorPrefs.reputation) - 1 
+                    : Number(authorPrefs.reputation) + 1
+            });
+                voteAction = "Vote Withdrawn";
+            } else {
+               await databases.deleteDocument(db, voteCollection, existingVote.$id);
+               doc = await databases.createDocument(db, voteCollection, ID.unique(), {
                 type,
                 typeId,
                 voteStatus,
                 votedById
+               });
+               
+               await users.updatePrefs<UserPrefs>(authorId, {
+                reputation: 
+                    existingVote.voteStatus === "upvoted" 
+                    ? Number(authorPrefs.reputation) + 2 
+                    : Number(authorPrefs.reputation) - 2
             });
+                voteAction = "Vote Status Updated";
+            }
 
-            //Increase or decrease reputation
-            const questionOrAnswer = await databases.getDocument(
-                db,
-                type === "question" ? questionCollection : answerCollection,
-                typeId
-            );
+        } else {
+            // First time voting
+            doc = await databases.createDocument(db, voteCollection, ID.unique(), {
+                type,
+                typeId,
+                voteStatus,
+                votedById
+               });
 
-            const authorPrefs = await users.getPrefs<UserPrefs>(questionOrAnswer.authorId)
-
-            //if vote was already present
-            
-
+            await users.updatePrefs<UserPrefs>(authorId, {
+                reputation:
+                    voteStatus === "upvoted"
+                        ? Number(authorPrefs.reputation) + 1
+                        : Number(authorPrefs.reputation) - 1,
+            }); 
         }
 
         const [upvotes, downvotes] = await Promise.all(
@@ -68,7 +88,7 @@ export async function POST(request: NextRequest){
                    Query.equal("typeId",typeId),
                    Query.equal("votedById",votedById),
                    Query.equal("voteStatus","upvoted"),
-                   Query.limit(1),
+                   Query.limit(1), //check only if user upvoted or not, but not to check how many times he clicked the button
                 ]),
                 databases.listDocuments(db, voteCollection, [
                    Query.equal("type",type),
@@ -78,18 +98,20 @@ export async function POST(request: NextRequest){
                    Query.limit(1),
                 ])
             ]
-        )
+        );
 
         return NextResponse.json(
             {
-                data: {document: null, voteResult: upvotes.total = downvotes.total},
-                message: "vote handled"
+                data: {
+                    document: doc, 
+                    voteResult: upvotes.total - downvotes.total
+                },
+                message: voteAction
             },
             {
-                status: 200
+                status: 201
             }
-        )
-
+        );
     } catch (error: any) {
         return NextResponse.json(
             {
@@ -98,6 +120,6 @@ export async function POST(request: NextRequest){
             {
                 status: error?.status || error?.code || 500
             }
-        )
+        );
     }
 }
