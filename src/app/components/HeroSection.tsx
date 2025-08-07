7a71cc0f8d2ababa9e3d081e6641234aced65628
ritanchmehta <ritanch0001@gmail.com>
2025-08-07 23:22:04 +0530

    added app components and added QuestionCard to ui components

---
+++ src/app/components/HeroSection.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { HeroParallax } from "@/components/ui/hero-parallax";
+import { databases } from "@/models/server/config";
+import { db, questionAttachmentBucket, questionCollection } from "@/models/name";
+import { Query } from "node-appwrite";
+import slugify from "@/utils/slugify";
+import { storage } from "@/models/client/config";
+import HeroSectionHeader from "./HeroSectionHeader";
+
+export default async function HeroSection() {
+    const questions = await databases.listDocuments(db, questionCollection, [
+        Query.orderDesc("$createdAt"),
+        Query.limit(15),
+    ]);
+
+    return (
+       <>
+        <HeroSectionHeader />
+        <HeroParallax
+            products={questions.documents.map(q => ({
+                title: q.title || "Untitled",
+                link: `/questions/${q.$id}/${slugify(q.title || "untitled")}`,
+                thumbnail: q.attachmentId 
+                    ? storage.getFilePreview(questionAttachmentBucket, q.attachmentId)
+                    :"/placeholder.png"
+            }))}
+        />
+        </>
+    );
+}
